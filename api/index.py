7d533ddf67e947680bf13e7989eefa73b3e1a5dc
Mo Adeysey <mo.adeysey@gmail.com>
2025-03-10 00:49:37 +0100

    Add API folder for Vercel deployment

---
+++ api/index.py
@@ -0,0 +1,13 @@
+﻿from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+import os
+import sys
+
+# Legg til prosjektets rotmappe i sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Importer app fra app.main
+from app.main import app as fastapi_app
+
+# Eksporter app for Vercel
+app = fastapi_app
